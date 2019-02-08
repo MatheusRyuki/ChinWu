fe642a7ca9450877dcefee72243f18777dc3a92f
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-08 17:25:20 -0300

    Importação da Frase chamativa

---
+++ src/components/Home/Highlight.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ui container">
+    <h1>Lorem Ipsum</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <app-header></app-header>
     <app-menu></app-menu>
+    <app-highlight></app-highlight>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Home/Header.vue';
 import SubHeader from '@/components/Home/Subheader.vue';
+import Highlight from '@/components/Home/Highlight.vue';
 
 export default {
   components: {
     appHeader: Header,
-    appMenu: SubHeader
+    appMenu: SubHeader,
+    appHighlight: Highlight
   },
 };
 </script>
