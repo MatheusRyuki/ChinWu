ced3e689e105645d26340ca7b76db1d29dd470e7
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-08 17:16:35 -0300

    Header do Top pronto

---
+++ src/components/Home/Subheader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui container">
+    <div class="ui secondary menu">
+      <a class="item">
+        {{Home}}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Home: "Home"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header></app-header>
+    <app-menu></app-menu>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '@/components/Home/Header.vue';
+import SubHeader from '@/components/Home/Subheader.vue';
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld,
+    appHeader: Header,
+    appMenu: SubHeader
   },
 };
 </script>
