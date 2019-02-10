e5ecf7d47c29a24da42ddb07fc491250d5909645
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-10 13:30:40 -0300

    Finalização Dropdown de Linguagem

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -3,9 +3,16 @@
     <div class="ui container">
       <div class="ui secondary menu">
         <a class="browse item">
-        <i class="teal globe icon"></i>
-        <span>{{language}}</span>
+            <i class="teal globe icon"></i>
+            <div class="ui simple dropdown">
+    <div class="default text">{{language}}</div>
         <i class="teal dropdown icon"></i>
+    <div class="menu">
+    <div class="item">{{pt}}<i class="br flag"></i></div>
+    <div class="item">{{en}}<i class="gb flag"></i></div>
+    <div class="item">{{es}} <i class="es flag"></i></div>
+  </div>
+              </div>
       </a>
        <div class="right menu">
          <a class="item">
@@ -22,14 +29,23 @@
 </template>
 
 <script>
+
+
 export default {
   data() {
     return {
       language: 'Idioma',
       login: 'Login',
       register: 'Registrar-se',
+      pt: 'Português  ',
+      en: 'Inglês  ',
+      es: 'Espanhol  '
     };
   },
+  mounted() {
+    // eslint-disable-next-line
+    $(".ui.dropdown").dropdown();
+  }
 };
 </script>
 
