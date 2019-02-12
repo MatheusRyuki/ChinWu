e976cde39224c845befe2c83fe21f1dd2e540c69
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-12 15:21:52 -0300

    Componente de Cards dos serviços

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <app-header></app-header>
+    <app-cards></app-cards>
   </div>
 </template>
 
 <script>
 import Header from '@/components/About/Header.vue';
+import Cards from '@/components/About/Cards.vue';
 
 export default {
   components: {
     appHeader: Header,
+    appCards: Cards,
   },
 };
 </script>
