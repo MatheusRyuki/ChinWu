a5e03770ddd5016f0ef7712d6a6f97c7d113fc26
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-14 16:14:41 -0300

    Cabeçalho na segunda página

---
+++ src/components/About/Subheader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="menu">
+    <div class="ui container">
+      <div class="ui stackable container secondary menu">
+        <a class="item">
+          <img class="ui tiny image" src="@/assets/logo.jpeg">
+        </a>
+         <div class="right menu">
+        <router-link :to="{ name: 'home'}" class="item">
+          {{Home}}
+        </router-link>
+          <a class="item">
+          {{About}}
+  </a>
+        <a class="item">
+          {{Courses}}
+        </a>
+        <a class="item">
+          {{Gallery}}
+        </a>
+        <a class="item">
+          {{Blog}}
+        </a>
+        <a class="item">
+          {{Contact}}
+        </a>
+        <a class="item">
+          {{Membership}}
+        </a>
+        <div class="item">
+      <div class="ui icon input">
+        <input type="text" placeholder="Pesquise algo...">
+        <i class="search link icon"></i>
+      </div>
+    </div>
+    </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Home: 'Home',
+      About: 'Sobre Nós',
+      Courses: 'Cursos',
+      Gallery: 'Galeria',
+      Blog: 'Blog',
+      Contact: 'Contato',
+      Membership: 'Franquias',
+    };
+  },
+};
+</script>
+
+<style scoped>
+#menu {
+  border-bottom: 1px blue solid;
+}
+.ui.secondary.menu .item {
+  font-weight: bolder;
+}
+.ui.secondary.menu .item:hover {
+  filter: brightness(75%);
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
+    <app-cabeca></app-cabeca>
+    <app-subheader></app-subheader>
     <app-header></app-header>
     <app-cards></app-cards>
   </div>
 </template>
 
 <script>
-import Header from '@/components/About/Header.vue';
+import Header2 from '@/components/About/Header.vue';
 import Cards from '@/components/About/Cards.vue';
+import Header from '@/components/About/Header0.vue';
+import SubHeader from '@/components/About/Subheader.vue';
 
 export default {
   components: {
-    appHeader: Header,
+    appHeader: Header2,
     appCards: Cards,
+    appCabeca: Header,
+    appSubheader: SubHeader,
   },
 };
 </script>
