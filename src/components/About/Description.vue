b59b8f3be2a83a240db31222ead979417e0a1f49
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-14 16:22:01 -0300

    Título inicial

--- src/components/About/Description.vue
+++ src/components/About/Description.vue
@@ -1,13 +1,29 @@
 <template lang="html">
 <div class="ui container">
-  <h1>teste</h1>
+  <h4 class="ui header blue">{{descricao}}</h4>
+  <h1 class="ui header">{{about}}</h1>
 </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      descricao: 'Nossa História',
+      about: 'Sobre nós',
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+h4.ui.header {
+  margin-top: 5%;
+}
+h1.ui.header {
+  margin-top: -2%;
+   border-bottom: 2px solid blue;
+display: inline-block;
+line-height: 2;
+}
 </style>
