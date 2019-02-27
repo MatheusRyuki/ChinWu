e4fc75bde2373d61664bb822757e8c760a46ca79
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-27 15:42:27 -0300

    Finalizado sobre nós

--- src/components/About/Videos.vue
+++ src/components/About/Videos.vue
@@ -3,6 +3,7 @@
     <div id="text">
       <h2 class="ui header text-light ">{{title}}</h2>
       <h1 class="ui header text-light">{{message}}</h1>
+      <button id="button" class="ui primary labeled icon button"><i class="play icon"></i>{{information}}</button>
     </div>
     <video class="ui fluid image"
   src="@/assets/Kungfu.mp4"
@@ -17,9 +18,10 @@ export default {
   data() {
     return {
       title: 'Venha treinar conosco',
-      message: 'Participe agora da melhor experiência de Kung Fu'
+      message: 'Participe agora da melhor experiência de Kung Fu',
+      information: 'Veja nossos vídeos',
     };
-  }
+  },
 };
 </script>
 
@@ -28,11 +30,14 @@ export default {
   margin-top: 5%;
   filter: brightness(25%);
 }
+#button {
+  margin-top: 5px;
+}
 #text {
   margin: 0 auto;
   z-index: 2;
   width: 100%;
-  margin-top: 20%;
+  margin-top: 25%;
   position: absolute;
 }
 .text-light {
