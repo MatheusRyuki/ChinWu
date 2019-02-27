b5bbd6d0e3922f63512ad8f71183712a69863ed7
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-02-27 16:26:14 -0300

    Estrutura do Componente Cursos construida, cartões

---
+++ src/components/Courses/Cards.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="ui container">
+    <div class="ui grid">
+      <div class="three column row">
+          <div class="column">
+            <div class="ui card">
+              <div class="content">
+        <i class="big user icon"></i>
+            <div class="header">{{courses}}</div>
+            <div class="description">
+              <p>{{descriptionCourses}}</p>
+            </div>
+          </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui card">
+              <div class="content">
+        <i class="big images outline icon"></i>
+            <div class="header">{{gallery}}</div>
+            <div class="description">
+              <p>{{descriptionGallery}}</p>
+            </div>
+          </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ui card">
+              <div class="content">
+      <i class="big briefcase icon"></i>
+            <div class="header">{{group}}</div>
+            <div class="description">
+              <p>{{descriptionGroup}}</p>
+            </div>
+          </div>
+            </div>
+          </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -3,6 +3,7 @@
     <app-header></app-header>
     <app-menu></app-menu>
     <app-title></app-title>
+    <app-cards></app-cards>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import Header from '@/components/Courses/Header.vue';
 import Subheader from '@/components/Courses/Subheader.vue';
 import Title from '@/components/Courses/Title.vue';
+import Cards from '@/components/Courses/Cards.vue';
 
 export default {
   components: {
     appHeader: Header,
     appMenu: Subheader,
     appTitle: Title,
+    appCards: Cards,
   },
 };
 </script>
