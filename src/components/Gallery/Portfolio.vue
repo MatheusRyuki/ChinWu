b0da02d218540618d68b47509089aec8317b763e
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2019-03-01 17:25:25 -0300

    Título do Portfólio

---
+++ src/components/Gallery/Portfolio.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="ui container">
+    <div id="title" class="ui basic segment">
+      <h4 class="ui header blue">{{subtitle}}</h4>
+      <h1 class="ui header">{{title}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subtitle: 'veja nossos melhores momentos',
+      title: 'Portfólio',
+    };
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#title {
+  margin-top: 5%;
+}
+.ui.header.blue {
+  color: blue;
+  margin-bottom: 0%;
+}
+h1.ui.header {
+  margin-top: -1%;
+  border-bottom: 2px solid blue;
+  display: inline-block;
+  line-height: 2;
+}
+</style>
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -2,17 +2,20 @@
   <div>
     <app-header></app-header>
     <app-subheader></app-subheader>
+    <app-portfolio></app-portfolio>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Gallery/Header.vue';
 import Subheader from '@/components/Gallery/Subheader.vue';
+import Portfolio from '@/components/Gallery/Portfolio.vue';
 
 export default {
   components: {
     appHeader: Header,
     appSubheader: Subheader,
+    appPortfolio: Portfolio,
   }
 }
 </script>
